2c7880966698b258f5cd60a2261182a6319b2763
Prem <premkumar33652@gmail.com>
2026-03-07 23:03:01 +0530

    reactvite0.2

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,17 @@
 
 //import './App.css'
-
+import Demo from'./demo.jsx'
 function App() {
+
+  const username = "hello peoples"
+
+
+
   return (
     <>
+    <Demo/>
     <h1>hello prem</h1>
+    <h2>its demo {username}</h2>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import Demo from './demo'
+import App from './App.jsx'
+
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App</h1>
+//     </div>
+//   )
+// }
+
+// const anotherElement = (
+//   <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+const anotheruser = " rohit"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com' , target: '_blank'},'Click me to visit google',
+  anotheruser
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Demo />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App /> 
+  // </StrictMode>
+  reactElement
 )
