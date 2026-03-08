e382682df4c108d1cb3196689752cf5f47acecf2
Prem <premkumar33652@gmail.com>
2026-03-08 13:40:27 +0530

    counter01

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [counter,setCounter] = useState(15)
+
+
+  const addValue = ()=>{1
+    setCounter(counter+1)
+    //console.log("clicked", counter)
+  }
+
+  const removeValue = ()=>{
+    setCounter(counter - 1)
+    //console.log("clicked", counter)
+  }
+
+  return (
+    <>
+      <h1>i am the best</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
